d8eac59a70d670ca72f339cf481b0b354477555d
yunii118 <lyhee118@gmail.com>
2024-02-08 20:29:44 +0900

    setting: profile 설정

--- src/main/java/org/sundo/configs/DbConfig.java
+++ src/main/java/org/sundo/configs/DbConfig.java
@@ -6,6 +6,7 @@ import javax.persistence.Persistence;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,9 +17,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class DbConfig {
 	
 	@Bean
+	@Profile("dev1")
 	public EntityManagerFactory entityManagerFactory() {
-		return Persistence.createEntityManagerFactory("jpa_config");
+		return Persistence.createEntityManagerFactory("jpa_dev1");
 	}
+	@Bean
+	@Profile("dev2")
+	public EntityManagerFactory entityManagerFactory2() {
+		return Persistence.createEntityManagerFactory("jpa_dev2"); 
+	}
+	@Bean
+	@Profile("prod")
+	public EntityManagerFactory entityManagerFactory3() {
+		return Persistence.createEntityManagerFactory("jpa_prod");
+	}
+	
 	
 	@Bean
 	public EntityManager entityManager() {
