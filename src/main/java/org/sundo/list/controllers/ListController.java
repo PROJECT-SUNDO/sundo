1f611849829f980eb37e21c6efc919cfae2e9a3d
3un3un <abc1987042@naver.com>
2024-02-13 21:16:35 +0900

    design: front/list.html 작성

---
+++ src/main/java/org/sundo/list/controllers/ListController.java
@@ -0,0 +1,21 @@
+package org.sundo.list.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/list")
+@RequiredArgsConstructor
+public class ListController {
+	
+	@GetMapping
+	public String list(Model model) {
+		return "front/list/list";
+	}
+	
+
+}
