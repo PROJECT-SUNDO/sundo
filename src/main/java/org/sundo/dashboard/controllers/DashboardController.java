eb1f2e2fc601d2197f70ab1c6c08713779772e5b
3un3un <abc1987042@naver.com>
2024-02-15 16:05:22 +0900

    design: front/dashboard 페이지 작성 및 controller 연결중

--- src/main/java/org/sundo/dashboard/controllers/DashboardController.java
+++ src/main/java/org/sundo/dashboard/controllers/DashboardController.java
@@ -6,12 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/dashboard")
 @RequiredArgsConstructor
 public class DashboardController {
     @GetMapping
     public String dashboard(Model model){
+        List<String> addScript = new ArrayList<>();
+        addScript.add("dashboard/dashboard");
+        System.out.println("addScript = " + addScript);
+        model.addAttribute("addScript", addScript);
+
         return "front/dashboard/dashboard";
     }
 }
