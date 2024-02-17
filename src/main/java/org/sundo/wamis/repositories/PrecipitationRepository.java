ef602627fc9a26c45a8501e50c2ae5c156025ef5
yoonyeji12 <yoonyeji12@gmail.com>
2024-02-17 18:35:17 +0900

    feat : 강수량 데이터 API받아오기

--- src/main/java/org/sundo/wamis/repositories/PrecipitationRepository.java
+++ src/main/java/org/sundo/wamis/repositories/PrecipitationRepository.java
@@ -3,7 +3,10 @@ package org.sundo.wamis.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.sundo.wamis.entities.Precipitation;
-import org.sundo.wamis.entities.PrecipitationId;
 
-public interface PrecipitationRepository extends JpaRepository<Precipitation, PrecipitationId>, QuerydslPredicateExecutor<Precipitation> {
+/**
+ * 강수량 데이터
+ */
+public interface PrecipitationRepository extends JpaRepository<Precipitation, Long>,
+        QuerydslPredicateExecutor<Precipitation> {
 }
