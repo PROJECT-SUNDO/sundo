ac9ca6cd67cd1bd37be6d58a85d1b05468cfd204
dkumylove <dkumylove@gmail.com>
2024-02-20 16:02:37 +0900

    refactor: 코드 리팩토링

--- src/main/resources/static/front/js/dashboard/dashboard.js
+++ src/main/resources/static/front/js/dashboard/dashboard.js
@@ -1,8 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
 
-  <!-- 기본 막대그래프 -->
-  const myChart1 = document.getElementById('myChart1').getContext('2d');
-  const barChart = new Chart(myChart1, {
+  const myChartOne = document.getElementById('myChartOne').getContext('2d');
+  const barChart = new Chart(myChartOne, {
     type: 'bar',  // bar, pie, line, doughnut,polarArea
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -37,9 +36,8 @@ window.addEventListener("DOMContentLoaded", function() {
     },
   });
 
-  <!-- 기본 라인그래프 -->
-  const myChart2 = document.getElementById('myChart2').getContext('2d');
-  const lineChart = new Chart(myChart2, {
+  const myChartTwo = document.getElementById('myChartTwo').getContext('2d');
+  const lineChart = new Chart(myChartTwo, {
     type: 'line',  // bar, pie, line, doughnut,polarArea
     data: {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -62,145 +60,5 @@ window.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
-
-  <!-- 멀티 라인그래프 -->
-  const myChart3 = document.getElementById('myChart3').getContext('2d');
-  const multiLineChart = new Chart(myChart3, {
-    type: 'multi_line',  // bar, pie, line, doughnut,polarArea
-    data: {
-      lables: ['a', 'b', 'c', 'd', 'e'],
-      datasets: [{
-        label: 'Temperature °C',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        backgroundColor: 'rgba(255, 99, 132, 1)',
-        yAxisID: 'y',
-        data: [0, 1, 2, 3, 4, 5],
-        borderWidth: 1,
-      }, {
-        label: 'Pressure (kPa)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        backgroundColor: 'rgba(54, 162, 235, 1)',
-        yAxisID: 'y1',
-        data: [5, 6, 7, 8, 3, 1],
-        borderWidth: 1,
-      },
-      ],
-    },
-
-    options: {
-      responsive: true,
-      interaction: {
-        mode: 'index',
-        intersect: false,
-      },
-      stacked: false,
-      plugins: {
-        title: {
-          display: true,
-          text: 'Chart.js Line Chart - Multi Axis'
-        }
-      },
-      scales: {
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-        },
-        y1: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-
-          // grid line settings
-          grid: {
-            drawOnChartArea: false, // only want the grid lines for one axis to show up
-          },
-        },
-      }
-    }
-  });
-
-  <!-- 멀티 막대그래프 -->
-  const myChart1_1 = document.getElementById('myChart1_1').getContext('2d');
-  const multiBarChart = new Chart(myChart1_1, {
-    type: 'multi_bar',  // bar, pie, line, doughnut,polarArea
-    data: {
-      labels: ['A', 'B', 'C'],
-      datasets: [
-        {
-          label: 'y axis left',
-          yAxisID: 'y-left',
-          data: [10, 20, 30],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          borderWidth: 1
-        },
-        {
-          label: 'y axis right',
-          yAxisID: 'y-right',
-          data: [10000000, 5000000, 3000000],
-          backgroundColor: [
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        },
-      ]
-    },
-
-    options: {
-      responsive:true,
-      // maintainAspectRatio: false,
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: 'X Axis Title'
-          },
-        },
-        'y-left': {
-          type: 'linear',
-          position: 'left',
-          title: {
-            display: true,
-            text: 'Y Axis Left'
-          },
-          grid: {
-            display: false
-          },
-        },
-        'y-right': {
-          type: 'linear',
-          position: 'right',
-          title: {
-            display: true,
-            text: 'Y Axis Right'
-          },
-          ticks: {
-            callback: (val) => (val.toExponential())
-          },
-          grid: {
-            display: false
-          },
-        },
-      },
-    },
-  });
-
-
 
 });
