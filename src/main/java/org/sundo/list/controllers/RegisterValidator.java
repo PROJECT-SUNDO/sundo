67131dbcf278fceaa8f6973fe45e1ee1d28dbd90
yoonyeji12 <yoonyeji12@naver.com>
2024-02-24 13:18:27 +0900

    feat : 유효성 오류수정및 완료

--- src/main/java/org/sundo/list/controllers/RegisterValidator.java
+++ src/main/java/org/sundo/list/controllers/RegisterValidator.java
@@ -6,5 +6,16 @@ public interface RegisterValidator {
      * 비밀번호에 숫자 포함 여부
      *
      */
-    //default boolean num
+    default boolean numberCheck(String obscd) {
+        return obscd.matches(".*\\d*");
+    }
+
+    /**
+     * 비밀번호에 특수 문제 포함 여부
+     *
+     */
+    default boolean specialCharsCheck(String addr) {
+        String pattern = ".*[`-`]+.*";
+        return addr.matches(pattern);
+    }
 }
