62735dfed96579b1442cdc1ba0fb76b4ca6d7f98
3un3un <abc1987042@naver.com>
2024-02-26 09:03:18 +0900

    feat: 관측 정보 1H, 1D

--- src/main/resources/static/front/js/list/api.js
+++ src/main/resources/static/front/js/list/api.js
@@ -44,8 +44,8 @@ window.addEventListener("DOMContentLoaded", function() {
 
         console.log('url ' + url);
         try {
-            const res = await fetch(url);
-            const result = await res.json();
+            const res = await fetch(url); // 비동기(await)방식으로 api 데이터 요청
+            const result = await res.json(); // 응답데이터 -> JSON형식으로 파싱
 
             if (!result) {
                 return;
@@ -66,10 +66,10 @@ window.addEventListener("DOMContentLoaded", function() {
                     min = ymdhm.substring(8, 12);
                 }
 
-                let key = `${year}.${month}.${day}`;
+                let key = `${year}-${month}-${day}`;
 
                 switch (unit) {
-                    case "1D": key = `${year}.${month}`; break;
+                    case "1D": key = `${year}-${month}`; break;
                     case "MONTH": key = `${year}`;
                     case "YEAR": break;
                 }
@@ -140,6 +140,11 @@ window.addEventListener("DOMContentLoaded", function() {
             fixedHeader.textContent = "관측일시";
             headerRow.appendChild(fixedHeader);
 
+            const rfText = "강수량(mm)";
+            const wlText = "수위(m)";
+            const fwText = "유량(㎥/sec)";
+
+
 
             /* 10M */
             if (unit === '10M') {
@@ -147,11 +152,11 @@ window.addEventListener("DOMContentLoaded", function() {
                 const typeHeader = document.createElement("div");
                 typeHeader.classList.add("divTableHead");
                 if (type === 'rf') {
-                    typeHeader.textContent = "강수량";
+                    typeHeader.textContent = rfText;
                 } else if (type === 'wl') {
-                    typeHeader.textContent = "수위";
+                    typeHeader.textContent = wlText;
                 } else if (type === 'fw') {
-                    typeHeader.textContent = "유량";
+                    typeHeader.textContent = fwText;
                 }
                 headerRow.appendChild(typeHeader);
                 thead.appendChild(headerRow);
@@ -164,8 +169,8 @@ window.addEventListener("DOMContentLoaded", function() {
                         for (const dateKey in rowData) {
                             if (rowData.hasOwnProperty(dateKey)) {
                                 if (dateKey.includes(type)) {
-                                    // ex) wl_2350 -> 2350
-                                    const var_substring = dateKey.substring(3, 5) + ':' + dateKey.substring(5);
+                                    // ex) wl_2350 -> 2350   dateKey.substring(3, 5) + ':' + dateKey.substring(5);
+                                    const var_substring = `${dateKey.substring(3, 5)}:${dateKey.substring(5)}`;
                                     const combinedText = `${key} ${var_substring}`;
 
                                     const row = document.createElement("div");
@@ -188,9 +193,7 @@ window.addEventListener("DOMContentLoaded", function() {
                         }
                     }
                 }
-            }
-
-            else if(unit === '1H') {
+            } else if(unit === '1H') {
                 const firstData = statData[Object.keys(statData)[0]];
                 const head_keys = Object.keys(firstData).reverse();
                 for (const key of head_keys) {
@@ -236,11 +239,182 @@ window.addEventListener("DOMContentLoaded", function() {
                     }
 
                 }
+            } else if(unit === '1D') {
+                thead.innerHTML = "";
+                const typeHeader = document.createElement("div");
+                typeHeader.classList.add("divTableHead");
+                if (type === 'rf') {
+                    typeHeader.textContent = rfText;
+                } else if (type === 'wl') {
+                    typeHeader.textContent = wlText;
+                } else if (type === 'fw') {
+                    typeHeader.textContent = fwText;
+                }
+                headerRow.appendChild(typeHeader);
+                thead.appendChild(headerRow);
+
+                // tbody
+                tbody.innerHTML = "";
+                for (const key in statData) {
+                    if (statData.hasOwnProperty(key)) {
+                        const rowData = statData[key];
+                        for (const dateKey in rowData) {
+                            if (rowData.hasOwnProperty(dateKey)) {
+                                if (dateKey.includes(type)) {
+                                    // ex) wl_2350 -> 2350
+                                    const var_substring = `-${dateKey.substring(3)}`;
+                                    const combinedText = `${key}${var_substring}`;
+
+                                    const row = document.createElement("div");
+                                    row.classList.add("divTableRow");
+                                    // 날짜
+                                    const dateCell = document.createElement("div");
+                                    dateCell.classList.add("divTableCell");
+                                    dateCell.textContent = combinedText;
+                                    row.appendChild(dateCell);
+                                    // 데이터
+                                    const dataCell = document.createElement("div");
+                                    dataCell.classList.add("divTableCell");
+                                    dataCell.textContent = rowData[dateKey];
+                                    console.log('rowData[dateKey]' + rowData[dateKey]);
+                                    row.appendChild(dataCell);
+
+                                    tbody.appendChild(row);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else if(unit === 'MONTH') {
+                thead.innerHTML = "";
+                const typeHeader = document.createElement("div");
+                typeHeader.classList.add("divTableHead");
+                if (type === 'rf') {
+                    typeHeader.textContent = rfText;
+                } else if (type === 'wl') {
+                    typeHeader.textContent = wlText;
+                } else if (type === 'fw') {
+                    typeHeader.textContent = fwText;
+                }
+                headerRow.appendChild(typeHeader);
+                thead.appendChild(headerRow);
+
+                // tbody
+                tbody.innerHTML = "";
+                for (const key in statData) {
+                    if (statData.hasOwnProperty(key)) {
+                        const rowData = statData[key];
+                        for (const dateKey in rowData) {
+                            if (rowData.hasOwnProperty(dateKey)) {
+                                if (dateKey.includes(type)) {
+                                    // ex) wl_2350 -> 2350
+                                    const yearText = dateKey.substring(3, 7);
+                                    const monthText = dateKey.substring(8);
+                                    const var_substring = `${yearText}-${monthText}`;
+
+                                    const row = document.createElement("div");
+                                    row.classList.add("divTableRow");
+                                    // 날짜
+                                    const dateCell = document.createElement("div");
+                                    dateCell.classList.add("divTableCell");
+                                    dateCell.textContent = var_substring;
+                                    row.appendChild(dateCell);
+                                    // 데이터
+                                    const dataCell = document.createElement("div");
+                                    dataCell.classList.add("divTableCell");
+                                    // wl, fw는 평균 구하기
+                                    if(type === 'wl' || type === 'fw') {
+                                        const daysInMonth = getDaysInMonth(parseInt(yearText), parseInt(monthText));
+                                        rowData[dateKey] /= daysInMonth;
+                                        dataCell.textContent = rowData[dateKey].toFixed(2); // 두 자리까지 반올림
+                                    }
+                                    row.appendChild(dataCell);
+
+                                    tbody.appendChild(row);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else if(unit === 'YEAR') {
+                thead.innerHTML = "";
+                const typeHeader = document.createElement("div");
+                typeHeader.classList.add("divTableHead");
+                if (type === 'rf') {
+                    typeHeader.textContent = rfText;
+                } else if (type === 'wl') {
+                    typeHeader.textContent = wlText;
+                } else if (type === 'fw') {
+                    typeHeader.textContent = fwText;
+                }
+                headerRow.appendChild(typeHeader);
+                thead.appendChild(headerRow);
+
+                // tbody
+                tbody.innerHTML = "";
+                for (const key in statData) {
+                    if (statData.hasOwnProperty(key)) {
+                        const rowData = statData[key];
+                        for (const dateKey in rowData) {
+                            if (rowData.hasOwnProperty(dateKey)) {
+                                if (dateKey.includes(type)) {
+                                    const row = document.createElement("div");
+                                    row.classList.add("divTableRow");
+                                    // 날짜
+                                    const dateCell = document.createElement("div");
+                                    dateCell.classList.add("divTableCell");
+                                    dateCell.textContent = dateKey;
+                                    row.appendChild(dateCell);
+                                    // 데이터
+                                    const dataCell = document.createElement("div");
+                                    dataCell.classList.add("divTableCell");
+                                    // wl, fw는 평균 구하기
+                                    if(type === 'wl' || type === 'fw') {
+                                        const daysInMonth = getDaysInMonth(parseInt(yearText), parseInt(monthText));
+                                        rowData[dateKey] /= daysInMonth;
+                                        dataCell.textContent = rowData[dateKey].toFixed(2); // 두 자리까지 반올림
+                                    }
+                                    dataCell.textContent = rowData[dateKey];
+                                    console.log('rowData[dateKey]' + rowData[dateKey]);
+                                    row.appendChild(dataCell);
+
+                                    tbody.appendChild(row);
+                                }
+                            }
+                        }
+                    }
+                }
             }
+
 
 
         } catch (err) {
             console.error(err);
         }
     });
+
+
+    // 현재 월이 아니면 해당 월의 전체 날짜 출력, 현재 월이면 오늘까지 날짜 출력
+    function getDaysInMonth(year, month) {
+        // 현재 날짜
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+        const currentMonth = currentDate.getMonth() + 1;
+        const currentDay = currentDate.getDate();
+
+        let daysInTargetMonth = new Date(year, month, 0).getDate();
+        let daysUntilToday = 0;
+
+        if (year === currentYear && month === currentMonth) {
+            daysUntilToday = currentDay;
+        } else {
+            daysUntilToday = daysInTargetMonth;
+        }
+
+        return daysUntilToday;
+    }
+
+
+
+
 });
