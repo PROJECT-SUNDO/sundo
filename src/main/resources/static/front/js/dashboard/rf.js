d139e0c0715eb09355258f63cd97e36b74f7cd19
yhg1024 <dgr1024kr@naver.com>
2024-02-27 16:26:51 +0900

    feat : dashboard list css

--- src/main/resources/static/front/js/dashboard/rf.js
+++ src/main/resources/static/front/js/dashboard/rf.js
@@ -1,7 +1,9 @@
 window.addEventListener("DOMContentLoaded", function() {
-const myChart2 = document.getElementById('myChart').getContext('2d');
-      const lineChart = new Chart(myChart2, {
-        type: 'line',  // bar, pie, line, doughnut,polarArea
+
+const myChart = document.getElementById('myChart').getContext('2d');
+
+  const barChart = new Chart(myChart, {
+     type: 'line',  // bar, pie, line, doughnut,polarArea
         data: {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
           datasets: [{
@@ -13,6 +15,12 @@ const myChart2 = document.getElementById('myChart').getContext('2d');
           }]
         },
         options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'bottom',
+            },
+
           scales: {
             x: {
               beginAtZero: true
@@ -22,5 +30,39 @@ const myChart2 = document.getElementById('myChart').getContext('2d');
             }
           }
         }
-    });
+     }
+  });
+    /*type: 'bar',  // bar, pie, line, doughnut,polarArea
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: '막대차트테스트',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        },
+      },
+    },*/
+
 });
